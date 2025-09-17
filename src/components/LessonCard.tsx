10bdc72f3200aeb0ffc1755eca35e2c9494f5eac
Nithin Kumar <nithinkumarj.mail@gmail.com>
2025-09-17 20:43:41 +0530

    Updated LessonCard.tsx

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -1,4 +1,4 @@
-import { LucideIcon } from "lucide-react";
+import { DivideIcon as LucideIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
